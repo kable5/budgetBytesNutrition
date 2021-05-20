8ff8e619d37a08664c23965348978d6f6e5e8253
Kris Ables <krisables@gmail.com>
2021-05-20 11:04:20 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Budget Bytes Web Scraper
+
+This Python program scrapes a single recipe on Budget Bytes and places the nutritional info in an xls file. An example output xls file is included.
+
+#### Requirements
+- requests
+- BeautifulSoup
+- xlwt
+
+#### TODO
+-[ ] requirements.txt
+-[ ] Insert serving name
+-[ ] Implementation for crawling though all recipies on the site 
+-[ ] More robust documentation
+-[ ] Move to proper database instead of xls
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import xlwt
+
+
+def getInfo(soup):
+    nut_elems = soup.find_all(class_='wprm-nutrition-label-text-nutrition-value')
+    recipe = soup.find(class_='title')
+
+    for nut_elem in nut_elems:
+        print(nut_elem.text, end='\n' * 2)
+
+    print(recipe.text, end='\n' * 2)
+
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet("My Sheet")
+
+    ws.write(0, 1, "Servings")
+    ws.write(0, 2, "Calories")
+    ws.write(0, 3, "Carbohydrates")
+    ws.write(0, 4, "Protein")
+    ws.write(0, 5, "Fat")
+    ws.write(0, 6, "Sodium")
+    ws.write(0, 7, "Fiber")
+
+    ws.write(1, 0, recipe.text)
+    index = 1
+    for nut_elem in nut_elems:
+        ws.write(1, index, nut_elem.text)
+        index = index + 1
+
+    wb.save("nutritionalInfo.xls")
+
+
+if __name__ == '__main__':
+
+    URL = 'https://www.budgetbytes.com/southwest-tortilla-baked-eggs/'
+    page = requests.get(URL)
+    pageSoup = BeautifulSoup(page.content, 'html.parser')
+    getInfo(pageSoup)
